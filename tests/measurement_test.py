2b5295159ea8eab08313fb15fdf841a9657d588b
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-10-18 15:45:21 +0200

    pep8

--- tests/measurement_test.py
+++ tests/measurement_test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 import unittest
@@ -9,10 +10,10 @@ import numpy as np
 
 import imma.measure
 
+
 class MeasurementTest(unittest.TestCase):
 
     def test_neigh_matrix(self):
-
         data = np.zeros((3, 4), dtype=int)
         data[:3, 1] = 1
         data[2:, 2] = 2
@@ -25,12 +26,11 @@ class MeasurementTest(unittest.TestCase):
         self.assertEqual(nbm[3][0], nbm[0][3])
 
     def test_neigh_matrix_obj(self):
-
         data = np.zeros((3, 4), dtype=int)
         data[:3, 1] = 1
         data[2:, 2] = 2
         data[0, 1:] = 3
-    #
+        #
         nbm = imma.measure.NeighboorMatrix(data)
         ndn = nbm.to_ndarray()
         sum = np.sum(ndn)
@@ -51,9 +51,7 @@ class MeasurementTest(unittest.TestCase):
         # self.assertEquals(new_shape[1], data_out.shape[1])
         # self.assertEquals(new_shape[2], data_out.shape[2])
 
-
     def test_neigh_matrix_obj_with_high_number(self):
-
         data = np.zeros((3, 4), dtype=int)
         data[:3, 1] = 1
         data[2:, 2] = 2
@@ -76,6 +74,7 @@ class MeasurementTest(unittest.TestCase):
         six2zero_0 = ndn[inv_keys[6], inv_keys[0]]
         six2zero_1 = nbm.get(6, 0)
         self.assertEqual(six2zero_0, six2zero_1, "# 6 is neighboor of 0 ")
+
 
 if __name__ == "__main__":
     unittest.main()
